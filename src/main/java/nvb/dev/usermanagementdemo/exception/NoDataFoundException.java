39c575c30b4ac997de8b1d9d45901bcbca7fdc39
Erfan Navab <erfannavab@gmail.com>
2024-01-11 13:39:14 +0330

    added custom exceptions

---
+++ src/main/java/nvb/dev/usermanagementdemo/exception/NoDataFoundException.java
@@ -0,0 +1,9 @@
+package nvb.dev.usermanagementdemo.exception;
+
+public class NoDataFoundException extends RuntimeException {
+
+    public NoDataFoundException() {
+        super("No Data Found.");
+    }
+
+}
