891f8dc24fa6b91886b9418998e34531b36baa97
Erfan Navab <erfannavab@gmail.com>
2024-01-11 13:22:56 +0330

    added user repository

---
+++ src/main/java/nvb/dev/usermanagementdemo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package nvb.dev.usermanagementdemo.repository;
+
+import nvb.dev.usermanagementdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
