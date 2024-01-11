4d94c68016061060f5b90b61f64653fe0735ed9a
Erfan Navab <erfannavab@gmail.com>
2024-01-11 13:38:39 +0330

    contains exception constants

---
+++ src/main/java/nvb/dev/usermanagementdemo/constant/ExceptionMessage.java
@@ -0,0 +1,13 @@
+package nvb.dev.usermanagementdemo.constant;
+
+public class ExceptionMessage {
+
+    private ExceptionMessage() {
+    }
+
+    public static final String TIME_STAMP = "timestamp";
+    public static final String MESSAGE = "message";
+    public static final String STATUS = "status";
+    public static final String ERRORS = "errors";
+
+}
