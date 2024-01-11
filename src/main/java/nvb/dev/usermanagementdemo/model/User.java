881006aa8d37d3957311441a4031cddd11cea741
Erfan Navab <erfannavab@gmail.com>
2024-01-11 13:22:19 +0330

    added User entity

---
+++ src/main/java/nvb/dev/usermanagementdemo/model/User.java
@@ -0,0 +1,37 @@
+package nvb.dev.usermanagementdemo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+@Table(name = "tbl_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "firstName cannot be empty")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @NotEmpty(message = "lastName cannot be empty")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NotEmpty(message = "username cannot be empty")
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Range(min = 18, max = 55)
+    @Column(name = "age", nullable = false)
+    private int age;
+
+}
