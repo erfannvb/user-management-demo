0270db8aac728c52617461e52616dda275e061c9
Erfan Navab <erfannavab@gmail.com>
2024-01-12 21:08:53 +0330

    containing constant vars for the security

---
+++ src/main/java/nvb/dev/usermanagementdemo/constant/SecurityConstant.java
@@ -0,0 +1,11 @@
+package nvb.dev.usermanagementdemo.constant;
+
+public class SecurityConstant {
+
+    private SecurityConstant() {
+    }
+
+    public static final String ADMIN_ROLE = "ADMIN";
+    public static final String USER_ROLE = "USER";
+
+}
