71d6bc26ef7352e25931750424b77f71485b02d1
Erfan Navab <erfannavab@gmail.com>
2024-01-12 08:49:07 +0330

    added 3 methods

--- src/main/java/nvb/dev/usermanagementdemo/repository/AccountRepository.java
+++ src/main/java/nvb/dev/usermanagementdemo/repository/AccountRepository.java
@@ -3,7 +3,19 @@ package nvb.dev.usermanagementdemo.repository;
 import nvb.dev.usermanagementdemo.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByUserId(Long userId);
+
+    @Transactional
+    void deleteByUserId(Long userId);
+
+    List<Account> findAccountByUserId(Long userId);
+
 }
