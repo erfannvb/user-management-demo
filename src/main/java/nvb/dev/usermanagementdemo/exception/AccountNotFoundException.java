7015fa884ed3cb26bca626dfa1c96b3a4c41297a
Erfan Navab <erfannavab@gmail.com>
2024-01-12 08:48:25 +0330

    changed param variable name to id

--- src/main/java/nvb/dev/usermanagementdemo/exception/AccountNotFoundException.java
+++ src/main/java/nvb/dev/usermanagementdemo/exception/AccountNotFoundException.java
@@ -1,9 +1,7 @@
 package nvb.dev.usermanagementdemo.exception;
 
 public class AccountNotFoundException extends RuntimeException {
-
     public AccountNotFoundException(Long accountId) {
         super(String.format("Account with id %d does not exist.", accountId));
     }
-
 }
--- src/main/java/nvb/dev/usermanagementdemo/exception/UserNotFoundException.java
+++ src/main/java/nvb/dev/usermanagementdemo/exception/UserNotFoundException.java
@@ -1,9 +1,7 @@
 package nvb.dev.usermanagementdemo.exception;
 
 public class UserNotFoundException extends RuntimeException {
-
     public UserNotFoundException(Long id) {
         super(String.format("User with id %d does not exist.", id));
     }
-
 }
