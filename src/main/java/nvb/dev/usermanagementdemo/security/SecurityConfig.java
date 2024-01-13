bc6e5819793ebbad12b5757934961112aa1235cd
Erfan Navab <erfannavab@gmail.com>
2024-01-13 14:28:14 +0330

    removed unused import

--- src/main/java/nvb/dev/usermanagementdemo/security/SecurityConfig.java
+++ src/main/java/nvb/dev/usermanagementdemo/security/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static nvb.dev.usermanagementdemo.constant.SecurityConstant.REGISTER_PATH;
