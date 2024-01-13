2cf8e0eaa3ed3af6245738d473e08236ce309aed
Erfan Navab <erfannavab@gmail.com>
2024-01-13 11:46:18 +0330

    added AUTHORIZATION, BEARER, and SECRET_KEY constant variables

--- src/main/java/nvb/dev/usermanagementdemo/constant/SecurityConstant.java
+++ src/main/java/nvb/dev/usermanagementdemo/constant/SecurityConstant.java
@@ -5,6 +5,11 @@ public class SecurityConstant {
     private SecurityConstant() {
     }
 
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
     public static final String REGISTER_PATH = "/api/v1/user/register";
+    public static final int TOKEN_EXPIRATION = 7200000;
+    public static final String SECRET_KEY = "MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAfRjTOapkgHSKQ/mEyADXLdJ93BS46DRm\n" +
+            "N8TXIVDASV2K8m9mimhc6FanQswWqxzF1IJ6lKT7tgUXt+OnipxXmQIDAQAB";
 
 }
