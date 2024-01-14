4376db1f4994ee44c36747fa094a5171989667ba
Erfan Navab <erfannavab@gmail.com>
2024-01-14 09:08:27 +0330

    deleted unused exceptions

--- src/main/java/nvb/dev/usermanagementdemo/exception/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package nvb.dev.usermanagementdemo.exception;
-
-public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(Long accountId) {
-        super(String.format("Account with id %d does not exist.", accountId));
-    }
-}
--- src/main/java/nvb/dev/usermanagementdemo/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package nvb.dev.usermanagementdemo.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id) {
-        super(String.format("User with id %d does not exist.", id));
-    }
-}
