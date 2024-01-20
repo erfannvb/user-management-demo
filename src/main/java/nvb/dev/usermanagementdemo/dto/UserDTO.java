4c51d633e09a574d5242a7faf4e0bb9aeeb1c6fb
Erfan Navab <erfannavab@gmail.com>
2024-01-20 11:37:47 +0330

    added @Builder

--- src/main/java/nvb/dev/usermanagementdemo/dto/UserDTO.java
+++ src/main/java/nvb/dev/usermanagementdemo/dto/UserDTO.java
@@ -1,12 +1,14 @@
 package nvb.dev.usermanagementdemo.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class UserDTO {
 
     private Long userId;
