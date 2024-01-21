29d8b2b82a4c1c3f6b09dacbc3dfa19fa767ac2c
Erfan Navab <erfannavab@gmail.com>
2024-01-21 12:10:34 +0330

    added to show the info

---
+++ src/main/java/nvb/dev/usermanagementdemo/bootstrap/BootstrapData.java
@@ -0,0 +1,24 @@
+package nvb.dev.usermanagementdemo.bootstrap;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import nvb.dev.usermanagementdemo.config.InfoConfig;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+@Log
+public class BootstrapData implements CommandLineRunner {
+
+    private final InfoConfig infoConfig;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(String.format("This app is made by %s in %d, this is version %.1f",
+                infoConfig.getMaker(),
+                infoConfig.getYear(),
+                infoConfig.getVersion()
+        ));
+    }
+}
