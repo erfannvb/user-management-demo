b66e1d1a6ff4a4bb339514726074f18e057b5165
Erfan Navab <erfannavab@gmail.com>
2024-01-21 12:05:59 +0330

    removed command line runner

--- src/main/java/nvb/dev/usermanagementdemo/UserManagementDemoApplication.java
+++ src/main/java/nvb/dev/usermanagementdemo/UserManagementDemoApplication.java
@@ -1,28 +1,12 @@
 package nvb.dev.usermanagementdemo;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.java.Log;
-import nvb.dev.usermanagementdemo.config.InfoConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@AllArgsConstructor
-@Log
-public class UserManagementDemoApplication implements CommandLineRunner {
-
-    private final InfoConfig infoConfig;
+public class UserManagementDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserManagementDemoApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.info(String.format("This app is made by %s in %d, this is version %s",
-                infoConfig.getMaker(),
-                infoConfig.getYear(),
-                infoConfig.getVersion()));
     }
 }
