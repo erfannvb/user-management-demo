6f1749d97db2a7f69c58c2547f3920d4d5f985d8
Erfan Navab <erfannavab@gmail.com>
2024-01-21 12:02:03 +0330

    implemented command line runner to show the info

--- src/main/java/nvb/dev/usermanagementdemo/UserManagementDemoApplication.java
+++ src/main/java/nvb/dev/usermanagementdemo/UserManagementDemoApplication.java
@@ -1,13 +1,28 @@
 package nvb.dev.usermanagementdemo;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import nvb.dev.usermanagementdemo.config.InfoConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserManagementDemoApplication {
+@AllArgsConstructor
+@Log
+public class UserManagementDemoApplication implements CommandLineRunner {
+
+    private final InfoConfig infoConfig;
 
     public static void main(String[] args) {
         SpringApplication.run(UserManagementDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(String.format("This app is made by %s in %d, this is version %s",
+                infoConfig.getMaker(),
+                infoConfig.getYear(),
+                infoConfig.getVersion()));
+    }
 }
